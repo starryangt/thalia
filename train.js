4b249bd0a38cca05a6d3107719759de7d1fe3f01
Starr Yang <starryangt@gmail.com>
2020-01-02 23:35:01 -0500

    Training script now uses train.txt and test.txt

--- train.js
+++ train.js
@@ -12,29 +12,30 @@ const { Matrix } = matrix
 
 const file_path = './training_data/Custom/'
 
-let total_X = []
-let total_y = []
+let list_of_training = fs.readFileSync('./training_data/training.txt', 'utf-8')
+let list_of_test = fs.readFileSync('./training_data/test.txt', 'utf-8')
 
-let lmao = 0
-for(let f of fs.readdirSync('./training_data/Custom/')){
-    lmao += 1
-    let pre_json = fs.readFileSync(path.join(file_path, f), "utf-8")
-    let json = JSON.parse(pre_json)
-    
-    total_X.push(...(json.X.map(e => e)))
-    total_y.push(...json.y)
-    console.log("Finished loading " + f)
-    if (lmao > 100){
-        break
+function load_files(list_of_files){
+    let x = []
+    let y = []
+    for(let f of list_of_files){
+        let pre_json = fs.readFileSync(path.join(file_path, f + ".html.json"), "utf-8")
+        let json = JSON.parse(pre_json)
+        
+        x.push(...(json.X.map(e => e)))
+        y.push(...json.y)
     }
+
+    return [x, y]
 }
 
-let p = new PCA(total_X, { 'nCompNIPALS': 5, 'method': 'NIPALS'})
-let new_X = p.predict(total_X)
-console.log(new_X)
+let [training_X, training_y] = load_files(list_of_training.split("\n"))
+
+let p = new PCA(training_X, { 'nCompNIPALS': 5, 'method': 'NIPALS'})
+let new_X = p.predict(training_X)
 
 var options = {
-    seed: 3,
+    seed: 42,
     maxFeatures: 5,
     replacement: true,
     nEstimators: 10
@@ -43,22 +44,26 @@ var options = {
 let classifier = new RandomForestClassifier(options)
 console.log("Training...")
 let previous_time = Date.now()
-classifier.train(new_X, total_y)
+classifier.train(new_X, training_y)
 let diff = Date.now() - previous_time
-console.log("Finished training, took " + diff + " seconds")
-let result = classifier.predict(new_X)
+console.log("Finished training, took " + (diff / 1000) + " seconds")
 
 console.log("Testing training accuracy")
+let [test_X, test_y] = load_files(list_of_test.split("\n"))
+let new_PX = p.predict(test_X)
+let result = classifier.predict(new_PX)
 let correct = 0
 let total = 0
 for(let i = 0; i < result.length; i++){
-    if(result[i] === total_y[i]){
+    if(result[i] === test_y[i]){
         correct += 1
     }
     total += 1
 }
+console.log("Final Test Accuracy: " + (correct / total))
 
-console.log("Final Training Accuracy: " + (correct / total))
-
+let PCASerialized = JSON.stringify(p)
 let serialized = JSON.stringify(classifier)
-fs.writeFileSync('./model.json', serialized)
+let date = Date.now()
+fs.writeFileSync(`./${date}_RFModel`, serialized)
+fs.writeFileSync(`./${date}_PCAModel`, PCASerialized)
